ec7aae88ba41fb496da20ed7f71c359c3f510b79
Yuri Campos <ygsoares20@gmail.com>
2022-12-03 17:11:28 -0300

    indentar as res

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     if(url === '/')
     {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Enter Data</title></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type = "submit"></button></form></body>')
-    res.write('</html');
-    return res.end();
+        //responses
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Data</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type = "submit"></button></form></body>')
+        res.write('</html');
+        return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
